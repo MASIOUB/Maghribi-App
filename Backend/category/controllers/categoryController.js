44b4177aad782d2a18deb0c4ea91c2a289a3b8ff
MASIOUB <93975488+MASIOUB@users.noreply.github.com>
2023-04-05 09:40:23 +0000

    add some changes on category module

--- Backend/category/controllers/categoryController.js
+++ Backend/category/controllers/categoryController.js
@@ -1,41 +1,35 @@
 const Category = require('../models/categoryModel');
+const asyncHandler = require('express-async-handler');
 
 // @route POST /categories
-const createCategory = async (req, res) => {
-    const { name } = req.body;
-
-    if (!name) {
-        res.status(400).json({ message: 'Enter name of category'});
-        // throw new Error('Enter name of category')
+const createCategory = asyncHandler(async (req, res) => {
+    try {
+        const newCategory = await Category.create(req.body);
+        res.json(newCategory);
+    }
+    catch (error) {
+        throw new Error(error);
     };
-
-    const category = await Category.create({
-        name: name
-    });
-
-    res.status(200).json(category);
-}
+});
 
 // @route GET /categories
-const getAllCategories = async (req, res) => {
-    const categories = await Category.find();
-
-    res.status(200).json(categories);
-}
+const getAllCategories = asyncHandler(async (req, res) => {
+    const allCategories = await Category.find();
+    res.status(200).json(allCategories);
+});
 
 // @route PUT /categories/:id
-const updateCategory = async (req, res) => {
+const updateCategory = asyncHandler(async (req, res) => {
     const category = await Category.findById(req.params.id)
 
     if (!category) {
         return res.status(404).json({ message: 'Category not found'});
-        // throw new Error(`Category not found`)
     }
 
     const apdatedCategory = await Category.findByIdAndUpdate(req.params.id, req.body, { new: true });
 
     res.status(200).json(apdatedCategory);
-}
+});
 
 module.exports = {
     createCategory,
--- Backend/category/models/categoryModel.js
+++ Backend/category/models/categoryModel.js
@@ -4,6 +4,8 @@ const categorySchema = mongoose.Schema(
     {
         name: {
             type: String,
+            required: true,
+            unique: true,
         }
     },
     {
