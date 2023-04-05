cf327e3626f0e3ff0efd1668eb3300a71b0f259c
MASIOUB <93975488+MASIOUB@users.noreply.github.com>
2023-04-05 11:27:30 +0000

    add some changes to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,39 +1,35 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import decorImage from '../assets/images/moroccan-interior-design.jpg';
 import womenImage from '../assets/images/women-clothes.jpg';
 import menImage from '../assets/images/men-clothes.jpg';
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    await axios
+      .get('http://localhost:3000/products')
+      .then(res => {
+        setProducts(res.data);
+        console.log(res.data);
+      });
+  };
+
+
+  // const ProductsList = (products.length === 0) ? (
+  //   'There is no products yet'
+  // ) : (
+  //   products.map((product, key) => <PostCard post={product} key={key} />)
+  // )
+
   return (
     <div>
-      {/* <div className="text-center bg-gray-50 text-primary py-24 px-6">
-        <h1 className="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">The best offer on the moroccan <br /> market</h1>
-        <a className="inline-block px-7 py-3 mr-2 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">Shop now</a>
-      </div> */}
-
-      {/* <div>
-        <div className="grid-cols-3 py-20 px-10 space-y-2 lg:space-y-0 lg:grid lg:gap-3 lg:grid-rows-3">
-          <div className="w-full col-span-2 row-span-2 rounded">
-            <img src={decorImage} alt={"moroccan"} />
-          </div>
-          <div className="w-fullrounded">
-            <img src={womenImage} alt={"moroccan"} />
-          </div>
-          <div className="w-full rounded">
-            <img src={menImage} alt={"moroccan"} />
-          </div>
-          <div className="w-fullrounded">
-            <img src={decorImage} alt={"moroccan"} />
-          </div>
-          <div className="w-full rounded">
-            <img src={decorImage} alt={"moroccan"} />
-          </div>
-          <div className="w-full rounded">
-            <img src={decorImage} alt={"moroccan"} />
-          </div>
-        </div>
-      </div> */}
-
       <div className="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0">
         <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
           <svg
@@ -74,137 +70,33 @@ const Home = () => {
 
         <div className="relative m-3 flex flex-wrap mx-auto justify-center">
 
-          <div className="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
-            <div className="overflow-x-hidden rounded-2xl relative">
-              <img src={decorImage} alt={"moroccan"} />
-              <p className="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                </svg>
-              </p>
-            </div>
-            <div className="mt-4 pl-2 mb-2 flex justify-between ">
-              <div>
-                <p className="text-lg font-semibold text-gray-900 mb-0">Product Name</p>
-                <p className="text-md text-gray-800 mt-0">$340</p>
+          {(products.length === 0) ? (
+            'There is no products yet'
+          ) : products.map((product, key) => (
+            <div className="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
+              <div className="overflow-x-hidden rounded-2xl relative">
+                <img src={decorImage} alt={"moroccan"} />
+                <p className="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                  </svg>
+                </p>
               </div>
-              <div className="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="gray">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                </svg>
+              <div className="mt-4 pl-2 mb-2 flex justify-between ">
+                <div>
+                  <p className="text-lg font-semibold text-gray-900 mb-0">{product.name}</p>
+                  <p className="text-md text-gray-800 mt-0">{product.price} DH</p>
+                </div>
+                <div className="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="gray">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                  </svg>
+                </div>
               </div>
             </div>
-          </div>
+          ))}
 
-          <div className="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
-            <div className="overflow-x-hidden rounded-2xl relative">
-              <img src={decorImage} alt={"moroccan"} />
-              <p className="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                </svg>
-              </p>
-            </div>
-            <div className="mt-4 pl-2 mb-2 flex justify-between ">
-              <div>
-                <p className="text-lg font-semibold text-gray-900 mb-0">Product Name</p>
-                <p className="text-md text-gray-800 mt-0">$340</p>
-              </div>
-              <div className="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="gray">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                </svg>
-              </div>
-            </div>
-          </div>
 
-          <div className="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
-            <div className="overflow-x-hidden rounded-2xl relative">
-              <img src={decorImage} alt={"moroccan"} />
-              <p className="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                </svg>
-              </p>
-            </div>
-            <div className="mt-4 pl-2 mb-2 flex justify-between ">
-              <div>
-                <p className="text-lg font-semibold text-gray-900 mb-0">Product Name</p>
-                <p className="text-md text-gray-800 mt-0">$340</p>
-              </div>
-              <div className="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="gray">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                </svg>
-              </div>
-            </div>
-          </div>
-
-          <div className="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
-            <div className="overflow-x-hidden rounded-2xl relative">
-              <img src={decorImage} alt={"moroccan"} />
-              <p className="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                </svg>
-              </p>
-            </div>
-            <div className="mt-4 pl-2 mb-2 flex justify-between ">
-              <div>
-                <p className="text-lg font-semibold text-gray-900 mb-0">Product Name</p>
-                <p className="text-md text-gray-800 mt-0">$340</p>
-              </div>
-              <div className="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="gray">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                </svg>
-              </div>
-            </div>
-          </div>
-
-          <div className="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
-            <div className="overflow-x-hidden rounded-2xl relative">
-              <img src={decorImage} alt={"moroccan"} />
-              <p className="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                </svg>
-              </p>
-            </div>
-            <div className="mt-4 pl-2 mb-2 flex justify-between ">
-              <div>
-                <p className="text-lg font-semibold text-gray-900 mb-0">Product Name</p>
-                <p className="text-md text-gray-800 mt-0">$340</p>
-              </div>
-              <div className="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="gray">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                </svg>
-              </div>
-            </div>
-          </div>
-
-          <div className="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
-            <div className="overflow-x-hidden rounded-2xl relative">
-              <img src={decorImage} alt={"moroccan"} />
-              <p className="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                </svg>
-              </p>
-            </div>
-            <div className="mt-4 pl-2 mb-2 flex justify-between ">
-              <div>
-                <p className="text-lg font-semibold text-gray-900 mb-0">Product Name</p>
-                <p className="text-md text-gray-800 mt-0">$340</p>
-              </div>
-              <div className="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="gray">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                </svg>
-              </div>
-            </div>
-          </div>
 
         </div>
       </div>
